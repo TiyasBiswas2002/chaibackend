4bfa1307038c876ea3bd9b50c975f5e8bd0d9321
Tiyas Biswas <tiyasbiswas91@gmail.com>
2024-01-30 23:33:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+    res.send('Tiyas Biswas')
+  })
+  app.get('/youtube', (req, res) => {
+    res.send('<h1>Chai aur code</h1>')
+  })
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
